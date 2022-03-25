1945aeb5418328f2e48185b5ee1d43954d7a9af8
KaoriAnimu <mkaorirahadj89@gmail.com>
2022-03-25 17:47:52 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TPKPL5
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            SayaTubeVideo saya = new SayaTubeVideo(" Tutorial Design By Contract - Moch. Kaori Rahadian ") ;
+
+            saya.IncreasePlayCount(0) ;
+
+            saya.PrintVideoDetails() ;
+
+            Console.WriteLine() ;
+
+            SayaTubeVideo saya1 = new SayaTubeVideo(" Moch. Kaori Rahadian ") ;
+
+            saya1.IncreasePlayCount(1000000000) ;
+
+            saya1.PrintVideoDetails() ;
+
+        }
+    }
+}
